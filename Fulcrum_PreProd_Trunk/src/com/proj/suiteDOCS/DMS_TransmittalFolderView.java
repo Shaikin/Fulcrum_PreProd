44ff7ce94b076141ff601d1451d23c0868812825
Anupama <anupama.thumrugoti@gmail.com>
2016-10-12 13:20:53 +0530

    Added DMS_TransmittalFOlderView to navigate to an existing DMSLibrary and add a new Transmittal folder view

---
+++ Fulcrum_PreProd_Trunk/src/com/proj/suiteDOCS/DMS_TransmittalFolderView.java
@@ -0,0 +1,70 @@
+package com.proj.suiteDOCS;
+import java.util.Hashtable;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import com.frw.Constants.Constants_FRMWRK;
+import com.frw.util.WaitUtil;
+import com.frw.wait.ExplicitWaitUtil;
+import com.proj.Constants.Constants_ConfigProperties;
+import com.proj.navigations.Navigations_Fulcrum;
+import com.proj.objectRepository.ObjRepository;
+import com.proj.suiteDOCS.reusables.SiteContents;
+import com.proj.util.CustomExceptions;
+import com.proj.util.TestExecutionUtil; 
+import com.proj.utilFulcrum.ApplicationMethods;
+import com.proj.utilFulcrum.WebTableUtil;
+
+public class DMS_TransmittalFolderView extends TestSuiteBase {
+
+	static String res="";
+
+	static String workFlow_Sitecontents = "Navigate to SiteContents-";
+	static String workFlow_DMS = "Open DMSDocLibrary-";
+	static String workFlow_CreateTxFolderView = "Create Tx Folder View-";
+	
+
+	@Test(dataProviderClass=com.proj.util.DataProviders.class,dataProvider="getData_Global")
+	public static void TestDMS_TransmittalFolderView(Hashtable<String,String>data
+			) throws Throwable{
+
+		System.out.println("In test");
+		logsObj.log("******************************************************");		
+		logsObj.log("Executing the test case: "+testcaseName);
+		try{
+
+			if(isBeforeMethodPass_docs==Constants_FRMWRK.FalseB){
+				CustomExceptions.Exit(testcaseName, "Before Method-Failure", "Due to above error in the Before Method cannot execute the test..");
+			}
+
+			logsObj.log("******************************************************");		
+			logsObj.log("Executing the test case: "+testcaseName);
+
+			Navigations_Fulcrum.Settings.navigateToSiteContents(driver_DOCS, workFlow_Sitecontents );
+			SiteContents.checkDMSLibraryAndOpen(driver_DOCS, workFlow_DMS , data);
+			SiteContents.clickFilesOnRibbon(driver_DOCS, workFlow_CreateTxFolderView);
+			SiteContents.clickNewDocument(driver_DOCS, workFlow_CreateTxFolderView);
+			SiteContents.clickTxFolderView(driver_DOCS, workFlow_CreateTxFolderView);
+			String Txname = SiteContents.txFolderName(driver_DOCS, workFlow_CreateTxFolderView, data);
+			SiteContents.mailIds(driver_DOCS, workFlow_CreateTxFolderView, data);
+			//SiteContents.clickSave(driver_DOCS, workFlow_CreateTxFolderView);
+			SiteContents.saveTxFolder(driver_DOCS, workFlow_CreateTxFolderView);
+			SiteContents.verifyAndClickTxFolderAdded(driver_DOCS, workFlow_CreateTxFolderView, Txname);
+			SiteContents.verifyDocAttachedInTx(driver_DOCS, testcaseName);
+			
+
+	
+
+		}catch(Throwable t){
+			CustomExceptions.final_catch_Reporting(driver_DOCS,refID,t);			
+		}
+
+		TestExecutionUtil.AssertTestStatus(isTestPass);
+
+	}
+}
+
+
+
+
